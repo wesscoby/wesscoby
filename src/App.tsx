fe75c5d9bcf0d79d02934a230cc2569e52a78e03
WessCoby <cobygiven@gmail.com>
2020-09-17 22:17:56 +0000

    Setup Google Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactGA from 'react-ga';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -6,6 +7,9 @@ import {
   Header, About, Resume, Portfolio, Testimonials, Contact, Footer
 } from './components';
 
+// Google Analytics
+ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS_ID!);
+ReactGA.pageview(window.location.pathname);
 
 toast.configure();
 function App() {
